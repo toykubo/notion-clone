6b9f40bd49b473a037ec05e8c547078a56c68bc1
Kubota Toyomasa <tkubota@seagreen.ocn.ne.jp>
2023-10-18 20:05:32 +0900

    setup first

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+
+export default function Home() {
+  return (
+    <div>
+      <Button className="bg-emerald-500 text-white">
+        Delete
+      </Button>
+    </div>
+    )
+}
